f10fc1bb038e7bd98ff856411b28ebbe4aa3eaa1
JackWFinlay <jfin404@aucklanduni.ac.nz>
2015-02-12 13:25:21 +1300

    Create script and test directories

---
+++ dwcp.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+in = $1
+out = $2
+now=(date +"%Y_%m_%d")
+
+for file in $in.csv
+do
+
+  file_datestamped = "${file%.*}$now"
+
+  sudo cp $file $out/$file_datestamped.csv
+  sudo mv $file $in/processed/$out/$file_datestamped.csv
+
+  sudo rm $file_datestamped.log $file_datestamped.bad
+
+done
