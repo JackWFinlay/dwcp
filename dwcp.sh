0b7a48274149e1c9e11d50d8f395359f04d571cc
JackWFinlay <jfin404@aucklanduni.ac.nz>
2015-02-12 13:30:58 +1300

    Fix dwcp.sh variables

--- dwcp.sh
+++ dwcp.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-in = $1
-out = $2
+in=$1
+out=$2
 now=(date +"%Y_%m_%d")
 
 for file in $in.csv
 do
 
-  file_datestamped = "${file%.*}$now"
+  file_datestamped="${file%.*}$now"
 
   sudo cp $file $out/$file_datestamped.csv
   sudo mv $file $in/processed/$out/$file_datestamped.csv
