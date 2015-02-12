2ea9eefed24e9bc5228bcde778b517cb30f5ba56
JackWFinlay <jfin404@aucklanduni.ac.nz>
2015-02-12 15:34:41 +1300

    Comment up dwcp.sh

--- dwcp.sh
+++ dwcp.sh
@@ -1,30 +1,45 @@
 #!/bin/bash
 
+# Usage: ./dwcp.sh <in> <out>
+
+# The directory to move files from.
 in=$1
+
+# The directory to move files to.
 out=$2
+
+# The current date.
 now=$(date +%Y_%m_%d)
 
 cd $in
 
+# Check if current directory contains any .CSV files.
 count=`ls -1 *.csv 2>/dev/null | wc -l`
 if [ $count != 0 ]
-then 
+then
+
+	# For every .CSV file in the directory.
 	for file in *.csv
 	do
+		# Generate file's new name.
 		file_datestamped="${file%.*}_${now}"
-	
-  		cp $file $out/${file_datestamped}.csv
 
+  	cp $file $out/${file_datestamped}.csv
+
+		# Create processed directory if it doesn't exist.
 		if [ ! -d "$in/processed" ]; then
- 			mkdir $in/processed 
+ 			mkdir $in/processed
 		fi
 
-  		mv $file $in/processed/${file_datestamped}.csv
+  	mv $file $in/processed/${file_datestamped}.csv
 
+		# remove .log and .bad files present from older versions of the file.
 		rm $out/$file_datestamped.log $out/$file_datestamped.bad
 
 	done
+
 else
-	echo "$in does not contain any .csv files"
+	# If a directory contains no .CSV files, don't do anything.
+	echo "Directory $in	does not contain any .csv files."
 
 fi
